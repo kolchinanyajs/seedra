000dc503dac8e6c6e697d05bc6b8642f11d2b188
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-04-21 11:38:21 +0300

    Change favicon

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const nextConfig = {
+  reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
     prependData: `@import "constants.scss"; @import "mixins.scss";`,
